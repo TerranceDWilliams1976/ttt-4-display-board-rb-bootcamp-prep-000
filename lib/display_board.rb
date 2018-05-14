579063cf6a36d7502f5ccf1af2d2c5a4e1a3bd11
Terrance Williams <terrance.d.williams1976@gmail.com>
2018-05-14 02:49:31 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,12 +2,20 @@
 # out the current state
 board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
-def display_board(board)
-puts"   |   |   "
-puts"-----------"
-puts"   |   |   "
-puts"-----------"
-puts"   |   |   "
+# def display_board(board)
+# puts"   |   |   "
+# puts"-----------"
+# puts"   |   |   "
+# puts"-----------"
+# puts"   |   |   "
+#
+# puts "#{board[0]} | #{board[1]} | #{board[2]} | #{board[3]} | #{board[4]} | #{board[5]} | #{board[6]} | #{board[7]} | #{board[8]} | #{board[9]}"
+# end
 
-puts "#{board[0]} | #{board[1]} | #{board[2]} | #{board[3]} | #{board[4]} | #{board[5]} | #{board[6]} | #{board[7]} | #{board[8]} | #{board[9]}"
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
